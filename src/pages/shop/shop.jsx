0c66e6ab10dee8a0cd79eb26f8534b005f4ee079
Barış Erzen <122172611+bariserzen@users.noreply.github.com>
2023-06-28 00:34:15 +0300

    development

---
+++ src/pages/shop/shop.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './shop.css';
+
+import {PRODUCTS} from '../../products';
+import Product from './product';
+
+
+const Shop = () => {
+  return (
+    <div className='shop'>
+
+        <div className='shopTitle'>
+            <h1>Shop</h1>
+        </div>
+
+        <div className='products'>
+
+          {PRODUCTS.map((product) => ( 
+          <Product data={product} />
+          
+          ))}
+        </div>
+
+    </div>
+  )
+}
+
+export default Shop;
